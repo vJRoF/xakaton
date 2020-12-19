36e7bcc40f7344dd6e2162e6356c9f898dfac098
Skiryuk Oleg <oskiryuk@beeline.ru>
2020-12-19 22:24:39 +0300

    send params in url

--- src/zakupki-gov/ClientApp/src/app/pages/main/grid/main-grid.component.ts
+++ src/zakupki-gov/ClientApp/src/app/pages/main/grid/main-grid.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { GridDataService } from '../../../core/services/grid-data.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { DataStateChangeEvent } from '@progress/kendo-angular-grid';
 import { IGridDataListRes } from '../../../core/models/grid-data.model';
-import { State } from '@progress/kendo-data-query';
+import { CompositeFilterDescriptor, FilterDescriptor, State } from '@progress/kendo-data-query';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+type CompositeOrSimpleFilter = FilterDescriptor | CompositeFilterDescriptor;
 
 @Component({
   selector: 'app-main-grid',
@@ -11,7 +15,9 @@ import { State } from '@progress/kendo-data-query';
   styleUrls: ['./main-grid.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MainGridComponent implements OnInit {
+export class MainGridComponent implements OnInit, OnDestroy {
+
+  private _queryParamsSub: Subscription;
 
   public state: State = {
     skip: 0,
@@ -19,7 +25,9 @@ export class MainGridComponent implements OnInit {
   };
 
   constructor(
-    private _gridDataService: GridDataService
+    private _gridDataService: GridDataService,
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute,
   ) {
     this.loadItems();
   }
@@ -33,11 +41,49 @@ export class MainGridComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this._queryParamsSub = this._activatedRoute.queryParams
+      .pipe(filter(res => !!res.take))
+      .subscribe((res) => {
+        this.state.filter = res.filter ? JSON.parse(res.filter) : res.filter;
+        this.state.sort = res.sort ? JSON.parse(res.sort) : res.sort;
+        this.state.group = res.group ? JSON.parse(res.group) : res.group;
+        this.state.skip = res.skip ? JSON.parse(res.skip) : res.skip;
+        this.state.take = res.take ? JSON.parse(res.take) : res.take;
+        this.loadItems();
+      });
   }
 
   public dataStateChange(state: DataStateChangeEvent): void {
-    this.state = state;
-    this.loadItems();
+    const gridFilter = state.filter ? { ...state.filter } : undefined;
+
+    if (gridFilter) {
+      this._transformFilters(gridFilter.filters);
+    }
+
+    this._router.navigate([], {
+      queryParams: {
+        take: state.take,
+        skip: state.skip,
+        sort: JSON.stringify(state.sort),
+        filter: JSON.stringify(gridFilter),
+        group: JSON.stringify(state.group)
+      }
+    });
+  }
+
+  private _transformFilters(filters: Array<CompositeOrSimpleFilter>): void {
+    if (filters) {
+      filters.forEach((f: CompositeOrSimpleFilter) => {
+        if ((f as CompositeFilterDescriptor).filters) {
+          this._transformFilters((f as CompositeFilterDescriptor).filters);
+        } else {
+          const v = (f as FilterDescriptor).value;
+          if (v && v instanceof Date) {
+            (f as FilterDescriptor).value = v.toISOString();
+          }
+        }
+      });
+    }
   }
 
   private loadItems(): void {
@@ -46,5 +92,9 @@ export class MainGridComponent implements OnInit {
 
   public exportToExcel(): void {
     this._gridDataService.exportToExcel(this.state);
+  }
+
+  ngOnDestroy() {
+    this._queryParamsSub.unsubscribe();
   }
 }
